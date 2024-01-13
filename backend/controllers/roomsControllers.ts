7828cab88df9b0fe37598acf198abcf8f461e95b
Huntertk <taufik.khan65@gmail.com>
2024-01-13 19:44:11 +0530

    Create catch async errors

--- backend/controllers/roomsControllers.ts
+++ backend/controllers/roomsControllers.ts
@@ -1,32 +1,36 @@
 import { NextRequest, NextResponse } from "next/server";
 import Room from "../models/room";
 import ErrorHandler from "../utils/errorHandler";
+import { catchAsyncErrors } from "../middlewares/catchAsyncErrors";
 
 //GET ALL Rooms    =>  /api/rooms
-export const allRooms = async (req:NextRequest) => {
-    const rooms = await Room.find()
-    return NextResponse.json({
-        success:true,
-        rooms
-    })
-}
+export const allRooms = catchAsyncErrors(
+    async (req:NextRequest) => {
+        const rooms = await Room.find()
+        return NextResponse.json({
+            success:true,
+            rooms
+        })
+    }
+) 
 
 //Create Room    =>  /api/admin/rooms
 
-export const newRoom = async (req:NextRequest) => {
-    const body = await req.json()
-    const room = await Room.create(body)
-
-    return NextResponse.json({
-        success:true,
-        room
-    })
-}
+export const newRoom = catchAsyncErrors(
+    async (req:NextRequest) => {
+        const body = await req.json()
+        const room = await Room.create(body)
+        
+        return NextResponse.json({
+            success:true,
+            room
+        })
+    }
+) 
 
 //GET Room Details   =>  /api/rooms/:id
-export const getRoomsDetails = async (req:NextRequest, {params}:{params:{id:string}}) => {
-    try {
-        throw new ErrorHandler("Helo", 404)
+export const getRoomsDetails = catchAsyncErrors ( 
+    async (req:NextRequest, {params}:{params:{id:string}}) => {
         const room = await Room.findById(params.id)
         if(!room){
             return NextResponse.json({
@@ -40,21 +44,13 @@ export const getRoomsDetails = async (req:NextRequest, {params}:{params:{id:stri
             success:true,
             room
         })
-    } catch (error: any) {
-        console.log(error.statusCode);
-        
-        return NextResponse.json({
-            message:error.message
-        },
-        {
-            status:500
-        })
-    }
-}
 
+}
+)
 
 //Update Room Details   =>  /api/admin/rooms/:id
-export const updateRoom = async (req:NextRequest, {params}:{params:{id:string}}) => {
+export const updateRoom = catchAsyncErrors( 
+    async (req:NextRequest, {params}:{params:{id:string}}) => {
     let room = await Room.findById(params.id)
     const body = await  req.json()
     if(!room){
@@ -72,25 +68,27 @@ export const updateRoom = async (req:NextRequest, {params}:{params:{id:string}})
         success:true,
         room
     })
-}
+})
 
 
 //Delete Room Details   =>  /api/admin/rooms/:id
-export const deleteRoom = async (req:NextRequest, {params}:{params:{id:string}}) => {
-    const room = await Room.findById(params.id)
-    const body = await  req.json()
-    if(!room){
+export const deleteRoom = catchAsyncErrors(
+    async (req:NextRequest, {params}:{params:{id:string}}) => {
+        const room = await Room.findById(params.id)
+        const body = await  req.json()
+        if(!room){
+            return NextResponse.json({
+                message:"Room Not Found"
+            },
+            {
+                status:404
+            })
+        }
+        
+        await room.deleteOne()
+        
         return NextResponse.json({
-            message:"Room Not Found"
-        },
-        {
-            status:404
+            success:true,
         })
     }
-
-   await room.deleteOne()
-    
-    return NextResponse.json({
-        success:true,
-    })
-}
+) 
