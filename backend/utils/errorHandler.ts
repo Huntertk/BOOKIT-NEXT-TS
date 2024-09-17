974c78955d0bad04677a625099dd548be1107a6e
Huntertk <taufik.khan65@gmail.com>
2024-09-17 19:18:29 +0530

    intial commit

--- backend/utils/errorHandler.ts
+++
@@ -1,9 +0,0 @@
-class ErrorHandler extends Error {
-    statusCode:number
-    constructor(errMessage:string, statusCode:number){
-        super(errMessage)
-        this.statusCode = statusCode
-    }
-}
-
-export default ErrorHandler
--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    env:{
-        DB_LOCAL_URI: "mongodb://127.0.0.1:27017/BOOKITDB",
-        DB_URI: ""
-    }
-}
-
-module.exports = nextConfig
